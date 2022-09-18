7fc048a6c74a8762d131461b3b0b748b41ea5129
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-09-18 03:54:32 +0100

    make sure the database has a lock

--- dynamic_website_reverse_proxy/database.py
+++ dynamic_website_reverse_proxy/database.py
@@ -4,6 +4,7 @@ A simple way to save and store the data of the server.
 """
 import pickle
 import os
+import threading
 
 
 class Database:
@@ -18,6 +19,7 @@ class Database:
         """
         self.file = file
         self.version = version
+        self.lock = threading.Lock()
         
     def _make_entry(self, value):
         """Create a database entry to store."""
@@ -32,15 +34,17 @@ class Database:
     def save(self, obj):
         """Save an object to the file."""
         os.makedirs(os.path.dirname(self.file), exist_ok=True)
-        with open(self.file, "wb") as f:
-            pickle.dump(self._make_entry(obj), f)
+        with self.lock:
+            with open(self.file, "wb") as f:
+                pickle.dump(self._make_entry(obj), f)
     
     def load(self):
         """Load an object from the file."""
         if not os.path.isfile(self.file):
             return None
-        with open(self.file, "rb") as f:
-            return self._read_entry(pickle.load(f))
+        with self.lock:
+            with open(self.file, "rb") as f:
+                return self._read_entry(pickle.load(f))
     
     def load_safely(self):
         """Load the object from the file but return None in case of an error."""
